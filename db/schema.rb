c8dd769f27055d5ea2d48d36b6681e23ef79b1c4
beccaelenzil <beccaelenzil@gmail.com>
2019-10-07 13:12:38 -0700

    database seeds

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_10_07_174733) do
+ActiveRecord::Schema.define(version: 2019_10_07_194038) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -28,6 +28,7 @@ ActiveRecord::Schema.define(version: 2019_10_07_174733) do
     t.datetime "updated_at", null: false
     t.string "description"
     t.bigint "author_id"
+    t.integer "publication_date"
     t.index ["author_id"], name: "index_books_on_author_id"
   end
 
