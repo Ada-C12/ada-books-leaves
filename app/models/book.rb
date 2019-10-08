efc258f633f09723b6c1fabc67e50bec4705c4ba
beccaelenzil <beccaelenzil@gmail.com>
2019-10-08 13:05:46 -0700

    validations, authors_controller show action

--- app/models/book.rb
+++ app/models/book.rb
@@ -1,6 +1,11 @@
 class Book < ApplicationRecord
   belongs_to :author
 
+  # validations
+  validates :title, presence: true
+  validates :publication_date, numericality: { only_integer: true, greater_than: 5}
+  #validates :description, presence: true, length: { minimum: 25 }
+
   def self.alpha_books
     return Book.order(title: :asc)
   end
