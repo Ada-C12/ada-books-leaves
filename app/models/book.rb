9331a887a9095cd7291d400c815803258c1a4680
Devin Helmgren <devincollege@gmail.com>
2019-10-14 10:26:59 -0700

    model testing start

--- app/models/book.rb
+++ app/models/book.rb
@@ -2,7 +2,7 @@ class Book < ApplicationRecord
   belongs_to :author
 
   # validations
-  validates :title, presence: true
+  validates :title, presence: true, uniqueness: true
   validates :publication_date, numericality: { only_integer: true, greater_than: 5}
   #validates :description, presence: true, length: { minimum: 25 }
 
--- test/models/book_test.rb
+++ test/models/book_test.rb
@@ -1,16 +1,44 @@
 require "test_helper"
 
 describe Book do
-  # describe "Validations" do
-  #   b = Book.new(title: "Good", author: variable_that_is_totally_valid, description: "fjkajjkldfsajlkfd")
+  describe 'validations' do
+    before do
+      # Arrange
+      author = Author.new(name: 'test author')
+      @book = Book.new(title: 'test book', author: author, publication_date: 2019)
+    end
 
-  #   b.title = nil
-  #   b.save
-  # end
+    it 'is valid when all fields are present' do
+      # Act
+      result = @book.valid?
 
-  # describe "custom methods" do
-  #   it "returns books in alphabetical order" do
+      # Assert
+      expect(result).must_equal true
+    end
 
-  #   end
-  # end
+    it 'is invalid without a title' do
+      @book.title = nil
+
+      expect(@book.valid?).must_equal false
+      expect(@book.errors.messages).must_include :title
+    end
+
+    it 'is invalid without a proper publication date' do
+      @book.publication_date = "abc"
+
+      expect(@book.valid?).must_equal false
+      expect(@book.errors.messages).must_include :publication_date
+
+
+      @book.publication_date = nil
+
+      expect(@book.valid?).must_equal false
+      expect(@book.errors.messages).must_include :publication_date
+
+      @book.publication_date = 5
+
+      expect(@book.valid?).must_equal false
+      expect(@book.errors.messages).must_include :publication_date
+    end
+  end
 end
